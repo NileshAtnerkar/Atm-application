53a6a8fc5148bd38deb5567c71c6d47db7c32256
Nilesh <nileshatnerkar11@gmail.com>
2021-12-26 17:06:02 +0530

    pin

--- pnchange.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-con=mysql.connector.connect(host="localhost",user="root",password="Nilesh@123",database="Atm")
-s = con.cursor()
-
-s.execute("update table account_details set")
-
-
